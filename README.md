ece335685e3d4217ac20e43d951ad4e9111a2c21
qux-bbb <1147635419@qq.com>
2019-09-13 22:53:08 +0800

    重写代码，添加原理描述

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # de-zip   
 Python脚本  
-对经过伪加密的zip文件进行处理
+对经过伪加密的zip文件进行处理  
+
+原理:  
+加密标志位在`general purpose bit flag`中，从后向前数，第一个bit为1，表示有加密  
+查找zip的加密标志位，将其置为0即可  
+
+```
+4.3.7  Local file header:
+
+    local file header signature     4 bytes  (0x04034b50)
+    version needed to extract       2 bytes
+    general purpose bit flag        2 bytes
+
+
+4.3.12  Central directory structure:
+
+    [central directory header 1]
+    .
+    .
+    . 
+    [central directory header n]
+    [digital signature] 
+
+    File header:
+
+    central file header signature   4 bytes  (0x02014b50)
+    version made by                 2 bytes
+    version needed to extract       2 bytes
+    general purpose bit flag        2 bytes
+
+
+4.4.4 general purpose bit flag: (2 bytes)
+
+    Bit 0: If set, indicates that the file is encrypted.
+```
+
+参考资料:  
+https://pkware.cachefly.net/webdocs/APPNOTE/APPNOTE-6.2.0.txt  
--- dzipfade.py
+++ dzipfade.py
@@ -1,10 +1,11 @@
 # coding:utf8
-# author: neo
+# author: qux
 '''
 zip伪加密去除脚本
 '''
 
 import sys
+import re
 
 def removefade(para1):
 	# 读取原zip文件
@@ -12,26 +13,36 @@ def removefade(para1):
 	zipfile_content = zipfile.read().encode('hex')
 	zipfile.close()
 
-	zf_len = len(zipfile_content)
-
-	# 查找加密标志位并处理
-	for i in xrange(zf_len):
-		comp_con = zipfile_content[i:i+8]
-		if comp_con == '504b0102':
-			zipfile_content = zipfile_content[:i+17] + '0' + zipfile_content[i+18:]
-		if comp_con == '504b0304':
-			zipfile_content = zipfile_content[:i+13] + '0' + zipfile_content[i+14:]
-
-
+	# 定位加密标志位并清零
+	# Local file header
+	about_global_enc_flag_re = r'504b0304.{8}'
+	match_contents = re.findall(about_global_enc_flag_re, zipfile_content)
+	if match_contents:
+		print '[*] Modify local file header flag:'
+		for match_content in match_contents:
+			modified_content = match_content[:12] + hex(int(match_content[12:14], 16) & 0b11111110)[2:].zfill(2) + match_content[14:]
+			print '    ' + match_content + ' --> ' + modified_content
+			zipfile_content = zipfile_content.replace(match_content, modified_content)
+	
+	# Central directory header
+	about_file_enc_flag_re = r'504b0102.{12}'
+	match_contents = re.findall(about_file_enc_flag_re, zipfile_content)
+	if match_contents:
+		print '[*] Modify central directory header flag:'
+		for match_content in match_contents:
+			modified_content = match_content[:16] + hex(int(match_content[16:18], 16) & 0b11111110)[2:].zfill(2) + match_content[18:]
+			print '    ' + match_content + ' --> ' + modified_content
+			zipfile_content = zipfile_content.replace(match_content, modified_content)
+	
 	# 将处理后内容写入新文件
-	newzip = open(para1[:-4] + 'repair.zip','wb')
+	newzip = open(para1[:-4] + '_repair.zip','wb')
 	newzip.write(zipfile_content.decode('hex'))
 	newzip.close()
 	print('Done')
 
 
 if __name__ == '__main__':
-	if(len(sys.argv) == 1):
+	if(len(sys.argv) != 2):
 		print('\nusage example:')
 		print(' python dzipfade.py a.zip\n')
 	else:
